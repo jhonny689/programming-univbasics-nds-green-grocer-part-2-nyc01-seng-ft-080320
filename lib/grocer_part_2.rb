b32247e26715635fb1f1e1eb8f6f669eb5128a5f
Jhonny Chamoun <jhonny.chamoun@gmail.com>
2020-07-28 17:50:40 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -12,7 +12,7 @@ def apply_coupons(cart, coupons)
     if available_coupon && val[:count] / available_coupon[:num] > 0
       cart[index][:count] = val[:count] % available_coupon[:num]
       coupon_item = {}
-      coupon_item[:item] = available_coupon[:item] + "W/COUPON"
+      coupon_item[:item] = available_coupon[:item] + " W/COUPON"
       coupon_item[:price] = available_coupon[:cost] / available_coupon[:num]
       coupon_item[:clearance] = val[:clearance]
       coupon_item[:count] = val[:count] / available_coupon[:num]
