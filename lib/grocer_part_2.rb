4e0bda5c6dc76bbddbf0615934b1facda7dd2933
Jhonny Chamoun <jhonny.chamoun@gmail.com>
2020-07-28 18:03:01 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -51,5 +51,5 @@ def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
   cart_with_coupons = apply_coupons(consolidated_cart, coupons)
   cart_with_clearance = apply_clearance(cart_with_coupons)
-  binding.pry
+  total = getTotal(cart_with_clearance)
 end
